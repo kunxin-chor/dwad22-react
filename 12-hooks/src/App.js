17f1b939834d00561e5aca16b621be948d78a98e
kunxin-chor <chorkunxin@yahoo.com>
2023-04-20 04:20:08 +0000

    Added hooks useMemo, useEffect

--- 12-hooks/src/App.js
+++ 12-hooks/src/App.js
@@ -1,27 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {useState, useCallback} from 'react'
+import { useState, useCallback, useEffect, useMemo } from 'react'
 function App() {
 
-    console.log("App");
+
     const [todos, setTodos] = useState([
-        {
-            "id": 101,
-            "name": "Walk the dog",
-            "done": false
-        },
-        {
-            "id": 102,
-            "name": "Clean the toilet",
-            "done": false
-        },
-        {
-            "id": 103,
-            "name": "Do the laundry",
-            "done": false
-        }
+
     ]);
-    
+
     const [newTodoName, setNewTodoName] = useState("");
+    const [filter, setFilter] = useState("all");
 
     // everytime we re-render we are creating a new version
     // of the `addNewTodo` function. This may lead to inefficienct because
@@ -49,33 +36,100 @@ function App() {
                 done: false
             }])
         }, [todos, newTodoName]  // <-- dependncy array. Only recreate addNewTodo if either the todos state
-                                 // changes or if the newTodoName state changes
+        // changes or if the newTodoName state changes
     )
 
-    const toggleTodo = useCallback( (taskIndex)=>{
-        const clonedTask = {...todos[taskIndex]};
+    const toggleTodo = useCallback((taskIndex) => {
+        const clonedTask = { ...todos[taskIndex] };
         clonedTask.done = !clonedTask.done;
-        setTodos([...todos.slice(0, taskIndex), clonedTask, ...todos.slice(taskIndex+1)]);
-    }, [todos])
-    
+        setTodos([...todos.slice(0, taskIndex), clonedTask, ...todos.slice(taskIndex + 1)]);
+    }, [todos]);
+
+    const saveTodos = useCallback(() => {
+        // we'll always have access to localstorage as long
+        // our React app is running on a browser
+        // first argument is the key to save to
+        // second argument is the data
+        localStorage.setItem('todos', JSON.stringify(todos)); // <--convert an array or object to a JSON string
+    }, [todos]);
+
+    const handleSelectFilter = useCallback((e) => {
+        setFilter(e.target.value);
+    }, [])
+
+    // useEffect allows us to create a "side effect"
+    // two arguments
+    // argument 1 - the function to run
+    // argument 2 - when is the function in argumen 1 ran
+    //   null --> the function is ran after EVERY re-render
+    //   [] --> the function is ran ONCE after the first render
+    //   [dependecies] --< the function is ran WHEN a dependency changes
+    useEffect(() => {
+        // once the component renders ONCE
+        // read in all the todos in a local storage
+        // and display it
+        // usually a side effect will make use of something outside of React
+
+        // retrieve all the tasks from our localstorage
+        const storedTodos = JSON.parse(localStorage.getItem('todos'));
+        setTodos(storedTodos);
+    }, []);
+
+    useEffect(()=>{
+        saveTodos();
+    }, [saveTodos])
+
+    let filteredTodos = useMemo(() => {
+        let results;
+        if (filter === "all") {
+            results = [...todos];
+        } else if (filter === "completed") {
+            results = todos.filter(t => t.done);
+        } else if (filter === "incomplete") {
+            results = todos.filter(t => !t.done);
+        }
+        return results;
+    }, [todos, filter]);
+
+
+
+
     return (
         <div className="container">
             <h1>My Todos</h1>
 
             <div className="mb-3">
-                <input type="text" className="form-control" value={newTodoName} onChange={(e)=>{
+                <input type="text" className="form-control" value={newTodoName} onChange={(e) => {
                     setNewTodoName(e.target.value);
-                }}/>
+                }} />
                 <button className="btn btn-success mt-2" onClick={addNewTodo}>Add</button>
             </div>
 
+            <div>
+                <button className="btn btn-primary mb-3" onClick={saveTodos}>Save</button>
+            </div>
+            <div>
+                <h2>Filters</h2>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="filterOptions" id="filterAll" value="all" onClick={handleSelectFilter} checked={filter === "all"} />
+                    <label className="form-check-label" htmlFor="filterAll">All</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="filterOptions" id="filterIncomplete" value="incomplete" onClick={handleSelectFilter} checked={filter === "incomplete"} />
+                    <label className="form-check-label" htmlFor="filterIncomplete">Incomplete</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="filterOptions" id="filterCompleted" value="completed" onClick={handleSelectFilter} checked={filter === "completed"} />
+                    <label className="form-check-label" htmlFor="filterCompleted">Completed</label>
+                </div>
+            </div>
             <ul className="list-group">
                 {
-                    todos.map( (t,index) => <li className="list-group-item">
+                    filteredTodos.map((t, index) => <li className="list-group-item">
                         {t.name}
-                        <input type="checkbox" checked={t.done} onChange={()=>{
+                        <input type="checkbox" checked={t.done} onChange={() => {
                             toggleTodo(index);
-                        }}className="ms-3 form-check-inline"/>
+                        }} className="ms-3 form-check-inline" />
                     </li>)
                 }
             </ul>
